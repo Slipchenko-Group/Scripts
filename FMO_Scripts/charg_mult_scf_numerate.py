9438e2dc202dccf5f64cd2f7c57ccf138c699754
Danil S Kaliakin <dkaliaki@halstead-fe00.rcac.purdue.edu>
2018-01-24 18:52:54 -0500

    Combinded Numerate and SCF/Mult Gen in One Master Script

--- FMO_Scripts/charg_mult_scf_numerate.py
+++ FMO_Scripts/charg_mult_scf_numerate.py
@@ -1,6 +1,9 @@
-# Script generates the arrays representing the SCF type 
-# and multiplicities of each fragment based on the array that 
+# First part of the script generates the arrays representing 
+# the SCF type and multiplicities of each fragment based on the array that 
 # represents the charges of each fragment. 
+
+import os
+import tempfile
 
 f1 = open('charg.txt', 'r')
 f2 = open('charg.txt.tmp', 'w')
@@ -56,3 +59,52 @@ for line in f1:
     f2.write(line.replace('MULT(RHF)', 'SCFFRG(1)'))
 f1.close()
 f2.close()
+
+os.remove('charg.txt.tmp2')
+os.remove('charg.txt.tmp')
+os.remove('scf.txt.tmp2')
+os.remove('scf.txt.tmp')
+
+# Second part of the script numerates nonzero fragments
+
+a1 = open('charg.txt', 'r')
+a2 = open('charg_list.txt', 'w')
+for line in a1:
+    a2.write(line.replace('ICHARG(1)=', ' '))
+a1.close()
+a2.close()
+b1 = open('charg_list.txt', 'r')
+b2 = open('charg_formated.txt', 'w')
+for line in b1:
+    b2.write(line.replace(',', ' '))
+b1.close()
+b2.close()
+arr = []
+inp = open ("charg_formated.txt","r")
+#read line into array 
+for line in inp.readlines():
+    # loop over the elemets, split by whitespace
+    for i in line.split():
+        # convert to integer and append to the list
+        arr.append(i)
+f4 = open('fragments_listing.txt', 'w')
+f3 = list(enumerate(arr))
+print >> f4, f3
+
+f5 = open('positive_charged_fragments_id.txt', 'w')
+for i, j in f3:
+    if j == '1':
+     print >> f5, i+1
+     
+f6 = open('negative_charged_fragments_id.txt', 'w')
+for i, j in f3:
+    if j == '-1':
+     print >> f6, i+1
+     
+f4.close()
+f5.close()
+f6.close()
+
+os.remove('charg_list.txt')
+os.remove('charg_formated.txt')
+os.remove('fragments_listing.txt')
